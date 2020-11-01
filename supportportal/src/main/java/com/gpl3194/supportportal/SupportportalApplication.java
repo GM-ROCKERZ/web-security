2f560acd44a55db851a18b014b4f02eaf0c5a6c4
gmrokz <gpl31.msh@gmail.com>
2020-11-01 09:37:13 +0530

    first commit

---
+++ supportportal/src/main/java/com/gpl3194/supportportal/SupportportalApplication.java
@@ -0,0 +1,13 @@
+package com.gpl3194.supportportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportportalApplication.class, args);
+	}
+
+}
---
+++ supportportal/src/main/java/com/gpl3194/supportportal/resources/UserResource.java
@@ -0,0 +1,17 @@
+package com.gpl3194.supportportal.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+	
+	@GetMapping("/home")
+	public String showUser() {
+		
+		return "application works";
+	}
+
+}
