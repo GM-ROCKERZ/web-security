a1784f2af646d3b46bdd5218d492e1af4dc90d31
Gopal Mishra <gopal_mishra@apple.com>
2020-11-03 19:25:25 +0530

    second commit

--- supportportal/src/main/java/com/gpl3194/supportportal/utility/JWTTokenProvider.java
+++ supportportal/src/main/java/com/gpl3194/supportportal/utility/JWTTokenProvider.java
@@ -1,17 +1,31 @@
 package com.gpl3194.supportportal.utility;
 
+import static com.gpl3194.supportportal.constant.SecurityConstant.AUTHORITIES;
+import static com.gpl3194.supportportal.constant.SecurityConstant.EXPIRATION_TIME;
+import static com.gpl3194.supportportal.constant.SecurityConstant.GET_ARRAYS_ADMINISTRATION;
+import static com.gpl3194.supportportal.constant.SecurityConstant.GET_ARRAYS_LLC;
+import static com.gpl3194.supportportal.constant.SecurityConstant.TOKEN_CANNOT_BE_VERIFIED;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import static com.gpl3194.supportportal.constant.SecurityConstant.*;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import com.gpl3194.supportportal.domain.UserPrincipal;
 
 public class JWTTokenProvider {
 	
-	@Value("jwt.secret")
+	@Value("${jwt.secret}")
 	private String secret;
 	
 	public String generateJwtToken(UserPrincipal userPrincipal) {
@@ -24,9 +38,40 @@ public class JWTTokenProvider {
 				.sign(Algorithm.HMAC512(secret.getBytes()));
 	}
 
-	private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
+	  public List<GrantedAuthority> getAuthorities(String token) {
+	        String[] claims = getClaimsFromToken(token);
+	        return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+	    }
 	
-		return null;
+	private String[] getClaimsFromToken(String token) {
+		
+		JWTVerifier verifier = getJWTVerifier();
+		
+		return verifier.verify(token).getClaim(AUTHORITIES).asArray(String.class);
+	}
+
+
+    private JWTVerifier getJWTVerifier() {
+        JWTVerifier verifier;
+        try {
+            Algorithm algorithm = HMAC512(secret);
+            verifier = JWT.require(algorithm).withIssuer(GET_ARRAYS_LLC).build();
+        }catch (JWTVerificationException exception) {
+            throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
+        }
+        return verifier;
+    }
+
+	private String[] getClaimsFromUser(UserPrincipal user) {
+	
+		List<String> authorties =new ArrayList<>();
+		
+		for(GrantedAuthority grantedAuthority:user.getAuthorities()) {
+			
+			authorties.add(grantedAuthority.getAuthority());
+		}
+		
+		return authorties.toArray(new String[0]);
 	}
 
 }
